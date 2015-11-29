47419ec987f14a316c0db319a98e451bb76f1725
Matt Quinn <matt@mattjquinn.com>
2015-11-29 22:52:57 +0000

    Modifying reset.py to reset single-node data_dir directories as well.

--- reset.py
+++ reset.py
@@ -6,6 +6,7 @@ from glob import glob
 print "Resetting nodes..."
 
 try:
+    os.remove('data_dir/hashchain.dat')
     os.remove('data_dirs/nodeA/hashchain.dat')
     os.remove('data_dirs/nodeB/hashchain.dat')
 except:
@@ -16,10 +17,14 @@ for f in glob('data_dirs/nodeA/documents/*'):
     os.remove(f)
 for f in glob('data_dirs/nodeB/documents/*'):
     os.remove(f)
+for f in glob('data_dir/documents/*'):
+    os.remove(f)
 print "Removed documents."
 
 for f in glob('data_dirs/nodeA/replicas/*'):
     os.remove(f)
 for f in glob('data_dirs/nodeB/replicas/*'):
+    os.remove(f)
+for f in glob('data_dir/replicas/*'):
     os.remove(f)
 print "Removed replicas."
